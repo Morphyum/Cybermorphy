09a2681d96dd051c1b88d248ccbd28ad6db5a4d4
Morphyum <zion@c-schwabe.de>
2014-06-19 19:30:55 +0200

    *Readded read Channels

--- src/de/morphyum/cybermorphy/Main.java
+++ src/de/morphyum/cybermorphy/Main.java
@@ -13,14 +13,12 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		SysTray tray = new SysTray();
-		ArrayList<String> channels = new ArrayList<String>();
-		channels.add("cybermorphy");
-		//HELPER.readChannels();
+		ArrayList<String> channels = HELPER.readChannels();
 		for (int i = 0; i < channels.size(); i++) {
 			if (isChannelWithoutCyber(channels.get(i)))
 				bots.add(newBot(channels.get(i)));
 		}
-		//bots.add(srlIrcBot());
+		bots.add(srlIrcBot());
 	}
 
 	public static void announce(String message) throws InterruptedException {
